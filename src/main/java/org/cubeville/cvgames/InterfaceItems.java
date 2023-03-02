e7560f8977a980118eb8556dee3c1c0a93305d13
ptown16 <peytontanzillo@icloud.com>
2023-03-01 20:55:26 -0800

    Update CVGames Interface Items

--- src/main/java/org/cubeville/cvgames/InterfaceItems.java
+++ src/main/java/org/cubeville/cvgames/InterfaceItems.java
@@ -6,7 +6,7 @@ import org.cubeville.cvgames.utils.GameUtils;
 public final class InterfaceItems {
     public static final ItemStack QUEUE_LEAVE_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTdjOGIwYjU2MmQ2YjBlMTBkZTUzMDNjZjgzNjk3ODExNGE0ZDA1ZDhmMTk3NDE2YTRhZDFhOWJkNDVlNTZjZCJ9fX0=", "§c§l§oLeave Queue §7§o(Right Click)");
     public static final ItemStack SPECTATE_LEAVE_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTdjOGIwYjU2MmQ2YjBlMTBkZTUzMDNjZjgzNjk3ODExNGE0ZDA1ZDhmMTk3NDE2YTRhZDFhOWJkNDVlNTZjZCJ9fX0=", "§c§l§oLeave §7§o(Right Click)");
-    public static final ItemStack SPECTATE_PLAYER_NAV_ITEM = GameUtils.customItem(Material.COMPASS, "§e§l§oSpectate Player §7§o(Right Click)");
-    public static final ItemStack TEAM_SELECTOR_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmM2YWQwYmMwNGI0NDdjZmIxMmViYjVkMDRmMGNmZWE1OTcyMWFjZGQ3MjhlOTZlYzJhY2YzOWM3MWIxMGE5NCJ9fX0=", "§b§l§oSelect Team §7§o(Right Click)");
-    public static final ItemStack RANDOMIZE_TEAM_ITEM = GameUtils.customItem(Material.BARRIER, "§c§l§oRandomized Team");
+    public static final ItemStack SPECTATE_PLAYER_NAV_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDhkNjIzMWUxYzYyOTliMzJlZGZjMTMxOTg5MjliMjY5ZDkyMWRmMmRmYzhlZTU2ZjA0ZTMyNjA0MDgxNzJmZSJ9fX0=", "§e§l§oSpectate Player §7§o(Right Click)");
+    public static final ItemStack TEAM_SELECTOR_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzAzZDdjZDAyZDUyMzU1ZGVjN2ZhNDE2YTkxZDY3ODQxZjg4MjQxNGYyYWM1NWE3NTZhMmViYjZlMDM0NmZlYiJ9fX0=", "§b§l§oSelect Team §7§o(Right Click)");
+    public static final ItemStack RANDOMIZE_TEAM_ITEM = GameUtils.customHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWFiNjEyZTQzNTU1ZTBlN2I5YzgwYzkyYzVmNzZlMDczMDlkNTQyNTM5ZmJhMzUzYmIxNGE3NmMwNTJlODQ4In19fQ==", "§e§l§oRandomized Team");
 }
